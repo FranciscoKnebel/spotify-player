8eeb2909fe66babe1eefb8437cb6fc56536046b5
Francisco Knebel <franciscopaivaknebel@gmail.com>
2017-08-19 02:42:23 -0300

    Added screenshot for readme.

--- src/Spotify.js
+++ src/Spotify.js
@@ -1,7 +1,7 @@
 import SpotifyWrapper from 'spotify-wrapper-api';
 
 const spotify = new SpotifyWrapper({
-	token: 'BQDZJFe5BR2sdeSq7mn9rDMSjj_8pWoNVvO-jWkUmrrebj4rRoM8StQs6E1xw4DY3R1pnjC4xtz8GhRfsB0s5Rk4vgewQMO2I3X86gqkP-uys7eQyDmxPTtiub31QtdWVOIiyucCwGhclqcbfghORA',
+	token: 'BQDAV9WCaBp5Xk-nksycGYpcXmt4M7ETHLX2jVk3f_zcKoQ62JziiojY4QoQAvt-9VBk1nLeN686ux46a5s3JzVnAhohLQEz5YE9a58brDPcWxS7geJ8I6HRr6fVV5JwbwOwvYg2JK0U1pN7qtpclQ',
 });
 
 export default spotify;
